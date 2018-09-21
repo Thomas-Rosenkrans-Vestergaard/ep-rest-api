478cd4704b009d5429dd2f776c8a00dfd0a79545
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-09-21 16:51:04 +0200

    added frontend

--- src/main/java/com/tvestergaard/rest/api/EventDTO.java
+++ src/main/java/com/tvestergaard/rest/api/EventDTO.java
@@ -2,15 +2,17 @@ package com.tvestergaard.rest.api;
 
 import com.tvestergaard.rest.entities.Event;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class EventDTO
 {
 
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
+
     public int    id;
     public String event;
     public String remark;
-    public Date   date;
+    public String date;
     public PetDTO pet;
 
     public EventDTO(Event event, boolean setPet)
@@ -18,7 +20,7 @@ public class EventDTO
         this.id = event.getId();
         this.event = event.getEvent();
         this.remark = event.getRemark();
-        this.date = event.getDate();
+        this.date = event.getDate() == null ? null : dateFormat.format(event.getDate());
         if (setPet)
             this.pet = new PetDTO(event.getPet(), true, false);
     }
--- src/main/java/com/tvestergaard/rest/api/PetDTO.java
+++ src/main/java/com/tvestergaard/rest/api/PetDTO.java
@@ -2,17 +2,20 @@ package com.tvestergaard.rest.api;
 
 import com.tvestergaard.rest.entities.Pet;
 
+import java.text.SimpleDateFormat;
 import java.util.Collection;
-import java.util.Date;
 import java.util.stream.Collectors;
 
 public class PetDTO
 {
+
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
+
     public int                  id;
     public String               name;
-    public Date                 birth;
+    public String               birth;
     public String               species;
-    public Date                 death;
+    public String               death;
     public OwnerDTO             owner;
     public Collection<EventDTO> events;
 
@@ -20,9 +23,9 @@ public class PetDTO
     {
         this.id = pet.getId();
         this.name = pet.getName();
-        this.birth = pet.getBirth();
+        this.birth = pet.getBirth() == null ? null : dateFormat.format(pet.getBirth());
         this.species = pet.getSpecies();
-        this.death = pet.getDeath();
+        this.death = pet.getDeath() == null ? null : dateFormat.format(pet.getDeath());
         if (setOwner)
             this.owner = new OwnerDTO(pet.getOwner(), false);
         if (setEvents)
